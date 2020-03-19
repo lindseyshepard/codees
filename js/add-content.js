6b12bb70eec3c52f478bfc1c27bd1c24ccf37c4e
Lindsey Shepard 🐧 <lishepar@microsoft.com>
2020-03-19 14:56:09 -0700

    loops and fixing src

--- js/add-content.js
+++ js/add-content.js
@@ -1,62 +1,71 @@
-var customerSaid = document.prompt("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
-if(customerSaid===yes){
-    return userInput();
+var customerSaid = prompt("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
+if (customerSaid === yes) {
+    // return userInput();
 }
-var choiceOfCoffee;
-var userInput = prompt(" Are you in the mood for something hot or something cold? ");
-if (userInput === null || userInput === "") {
-    choiceOfCoffee = "No coffee for you!";
-} else if (userInput === 'cold') {
-    prompt('You can choose an ice coffee or ice latte? ');
+
+function customerChoice() {
+    var userInput = prompt(" Are you in the mood for something hot or something cold? ");
+    if (userInput === null || userInput === "") {
+        choiceOfCoffee = "No coffee for you!";
+    } else if (userInput === 'cold') {
+        prompt('You can choose an ice coffee or ice latte? ');
         choiceOfCoffee = prompt.apply();
         return choiceOfCoffee;
-} else {
-prompt('we have hot!');
-}
-document.write(customerSaid());
-document.write('<h2>' + choiceOfCoffee + '</h2>');
-
-
-var customerTotal=0;
-var iceLattee= 3;
-var icecoffee=2.5;
-var hotcoffee=2;
-choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
-for (var i = 0; i < 3; i++){
-    if (i === iceLattee){
-customerTotal+=3;
-    }
-    else if (i === icecoffee){
-        customerTotal+=2.5;
-    }
-    else if (i === hotcoffee){
-        customerTotal+=2;
     } else {
-        return customerTotal;
+        prompt('we have hot!');
     }
-    
-    document.write('Your total is: $' + customerTotal);
+    document.write(customerSaid());
+    document.write('<h2>' + choiceOfCoffee + '</h2>');
+
 }
 
 
-if (userInput === icecoffee) {
-    confirm('Enjoy your ice coffee!');
-} else if (userInput = iceLattee) {
-    confirm("Enjoy your Lattee and have the best day!");
-} else if (userInput = hotcoffee) {
-    confirm('Here is your hot coffee! Stay warm.');
-} else {
-    document.write('Please pick an option from the menu. ');
+
+var customerTotal = 0;
+var iceLattee = 3;
+var icecoffee = 2.5;
+var hotcoffee = 2;
+var choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
+while (customerTotal === '' || isNaN(customerTotal)) {
+
+
+
+    for (var i = 0; i <= 3; i++) {
+        if (i == i[0]) {
+            customerTotal += 3;
+        }
+        else if (i === icecoffee) {
+            customerTotal += 2.5;
+        }
+        else if (i === hotcoffee) {
+            customerTotal += 2;
+        } else {
+            return customerTotal;
+        }
+
+        document.write('Your total is: $' + customerTotal);
+    }
 }
-document.write(userInput);
+customerChoice();
+
+// if (userInput === icecoffee) {
+//     confirm('Enjoy your ice coffee!');
+// } else if (userInput = iceLattee) {
+//     confirm("Enjoy your Lattee and have the best day!");
+// } else if (userInput = hotcoffee) {
+//     confirm('Here is your hot coffee! Stay warm.');
+// } else {
+//     document.write('Please pick an option from the menu. ');
+// }
+// document.write(userInput);
 
 
 
-// document.getElementById("ice").addEventListener("Click", function () {
-//     getCoffee(icecoffee);
-// })
-// document.getElementById("lattee").addEventListener("Click", function () {
-//     getCoffee(iceLattee);
+// // document.getElementById("ice").addEventListener("Click", function () {
+// //     getCoffee(icecoffee);
+// // })
+// // document.getElementById("lattee").addEventListener("Click", function () {
+// //     getCoffee(iceLattee);
 // })
 // document.getElementById("hot").addEventListener("Click", function () {
 //     getCoffee(hotcoffee);
