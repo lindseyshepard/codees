0e8a9396d8862d856003206fd78f50637f38c3d7
Lindsey Shepard 🐧 <lishepar@microsoft.com>
2020-03-18 17:19:09 -0700

    commit

---
+++ js/add-content.js
@@ -0,0 +1,29 @@
+document.write("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
+var userInput = document.write(" What type of beverage would you like? n\
+The options are ice coffee, ice lattee or you may have a classic hot n\
+coffee to keep warm? ");
+confirm(userInput===false);
+
+
+    if (userInput === icecoffee) {
+        confirm('Enjoy your ice coffee!');
+    } else if (userInput = iceLattee) {
+        confirm("Enjoy your Lattee and have the best day!");
+    } else if (userInput = hotcoffee) {
+        confirm('Here is your hot coffee! Stay warm.');
+    } else {
+        document.write('Please pick an option from the menu. ');
+    }
+
+
+
+
+// document.getElementById("ice").addEventListener("Click", function () {
+//     getCoffee(icecoffee);
+// })
+// document.getElementById("lattee").addEventListener("Click", function () {
+//     getCoffee(iceLattee);
+// })
+// document.getElementById("hot").addEventListener("Click", function () {
+//     getCoffee(hotcoffee);
+// })
