b5e2c05df8ed87fc8094aa6460aa08cbdfda65d4
Lindsey Shepard 🐧 <lishepar@microsoft.com>
2020-03-19 15:13:29 -0700

    loop

--- js/add-content.js
+++ js/add-content.js
@@ -1,53 +1,52 @@
-var customerSaid = prompt("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
-if (customerSaid === yes) {
-    // return userInput();
-}
+//var customerSaid = prompt("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
+//if (customerSaid === yes) {
+// return userInput();
+//}
 
-function customerChoice() {
-    var userInput = prompt(" Are you in the mood for something hot or something cold? ");
-    if (userInput === null || userInput === "") {
-        choiceOfCoffee = "No coffee for you!";
-    } else if (userInput === 'cold') {
-        prompt('You can choose an ice coffee or ice latte? ');
-        choiceOfCoffee = prompt.apply();
-        return choiceOfCoffee;
-    } else {
-        prompt('we have hot!');
+// function customerChoice() {
+//     var userInput = prompt(" Are you in the mood for something hot or something cold? ");
+//     if (userInput === null || userInput === "") {
+//         choiceOfCoffee = "No coffee for you!";
+//     } else if (userInput === 'cold') {
+//         prompt('You can choose an ice coffee or ice latte? ');
+//         choiceOfCoffee = prompt.apply();
+//         return choiceOfCoffee;
+//     } else {
+//         prompt('we have hot!');
+//     }
+//     document.write(customerSaid());
+//     document.write('<h2>' + choiceOfCoffee + '</h2>');
+
+// }
+
+
+var userChoice = function () {
+    var customerTotal = 0;
+    var amount = prompt('How many would you like?');
+    var iceLattee = 3;
+    var icecoffee = 2.5;
+    var hotcoffee = 2;
+    while (customerTotal === '' || isNaN(customerTotal)) {
+
+        var choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
+        for (var i = 0; i <= 3; i++) {
+            if (i == iceLattee) {
+                customerTotal += 3;
+            }
+            else if (i === icecoffee) {
+                customerTotal += 2.5;
+            }
+            else if (i === hotcoffee) {
+                customerTotal += 2;
+            } else {
+                return customerTotal;
+            }
+            document.write('Your total is: $' + customerTotal);
+            amount = customerTotal;
+        }
+        return customerTotal;
     }
-    document.write(customerSaid());
-    document.write('<h2>' + choiceOfCoffee + '</h2>');
-
 }
-
-
-
-var customerTotal = 0;
-var iceLattee = 3;
-var icecoffee = 2.5;
-var hotcoffee = 2;
-var choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
-while (customerTotal === '' || isNaN(customerTotal)) {
-
-
-
-    for (var i = 0; i <= 3; i++) {
-        if (i == i[0]) {
-            customerTotal += 3;
-        }
-        else if (i === icecoffee) {
-            customerTotal += 2.5;
-        }
-        else if (i === hotcoffee) {
-            customerTotal += 2;
-        } else {
-            return customerTotal;
-        }
-
-        document.write('Your total is: $' + customerTotal);
-    }
-}
-customerChoice();
-
 // if (userInput === icecoffee) {
 //     confirm('Enjoy your ice coffee!');
 // } else if (userInput = iceLattee) {
