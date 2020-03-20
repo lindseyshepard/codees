da1828ef759b2dafe6f4c97917a97291e3bf1a8a
Lindsey Shepard 🐧 <lishepar@microsoft.com>
2020-03-19 17:16:10 -0700

    loops

--- js/add-content.js
+++ js/add-content.js
@@ -1,4 +1,4 @@
-//var customerSaid = prompt("Hi! Welcome to Codees. To get started would you perfer coffee or something to nibble on?");
+
 //if (customerSaid === yes) {
 // return userInput();
 //}
@@ -20,33 +20,34 @@
 // }
 
 
-var userChoice = function () {
-    var customerTotal = 0;
-    var amount = prompt('How many would you like?');
-    var iceLattee = 3;
-    var icecoffee = 2.5;
-    var hotcoffee = 2;
-    while (customerTotal === '' || isNaN(customerTotal)) {
+// var userChoice = function () {
+//     var customerTotal = 0;
+//     var iceLattee = 3;
+//     var icecoffee = 2.5;
+//     var hotcoffee = 2;
+//     var order = prompt('What kind of coffee would you like?');
+//     while (customerTotal === '' || isNaN(customerTotal)) {
 
-        var choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
-        for (var i = 0; i <= 3; i++) {
-            if (i == iceLattee) {
-                customerTotal += 3;
-            }
-            else if (i === icecoffee) {
-                customerTotal += 2.5;
-            }
-            else if (i === hotcoffee) {
-                customerTotal += 2;
-            } else {
-                return customerTotal;
-            }
-            document.write('Your total is: $' + customerTotal);
-            amount = customerTotal;
-        }
-        return customerTotal;
-    }
-}
+
+//         var choiceOfCoffee = [iceLattee, icecoffee, hotcoffee];
+//         for (var i = 0; i <= 3; i++) {
+//             if (i == iceLattee) {
+//                 customerTotal += 3;
+//             }
+//             else if (i === icecoffee) {
+//                 customerTotal += 2.5;
+//             }
+//             else if (i === hotcoffee) {
+//                 customerTotal += 2;
+//             } else {
+//                 return customerTotal;
+//             }
+//             document.write('Your total is: $' + customerTotal);
+//             amount = customerTotal;
+//         }
+//         return customerTotal;
+//     }
+// }
 // if (userInput === icecoffee) {
 //     confirm('Enjoy your ice coffee!');
 // } else if (userInput = iceLattee) {
@@ -69,3 +70,49 @@ var userChoice = function () {
 // document.getElementById("hot").addEventListener("Click", function () {
 //     getCoffee(hotcoffee);
 // })
+var buyCoffee = prompt("Hi! Welcome to Codees. Would you like to buy a coffee?");
+
+while (buyCoffee !== "yes" || buyCoffee !== "no") {
+    var buyCoffee = prompt('Would you like to buy a coffee?');
+
+
+    if (buyCoffee === "yes") {
+        ask();
+    } else if (buyCoffee === "no") {
+        var exitGreeting = document.write('Have a great day!');
+    }
+}
+
+// coffee images
+//var icecoffee = <img src="http://1.bp.blogspot.com/-UV9fcFYETGc/ThYLf-46zMI/AAAAAAAAEXI/3FgR0-Y4ypg/s1600/coffee4.JPG"></img>
+//var iceLattee = <img src="http://www.bramptonfoods.ca/wp-content/uploads/2015/05/blended-ice-coffee.jpg"></img>
+//var hotcoffee = <img src="https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/58324-iStock-157528129.jpg?itok=kihwEBm5"></img>
+function ask() {
+
+
+    var perference = prompt('Would you like a hot coffee or an ice coffee/latte? ');
+    while (perference !== 'hot' || perference !== 'ice') {
+        var perference = ('please choose ice or hot');
+    }
+    if (perference === ice) {
+        var iceChoices = prompt('Ice coffee or ice lattee?')
+        if (iceChoices === icecoffee) {
+            document.createElement(icecoffee);
+        } else if (perference === iceLattee) {
+            document.createElement(iceLattee);
+        } else {
+            var iceChoices = prompt('please enter icecoffee or icelattee');
+        }
+        return iceChoices;
+    } else {
+        var hotChoice = prompt('How many hot coffees would you like? ');
+        var total = 0;
+        total += hotChoice;
+        document.write(total);
+        document.createElement(hotcoffee);
+
+
+    }
+}
+// document.createElement(icecoffee);
+//  document.write(icecoffee);
